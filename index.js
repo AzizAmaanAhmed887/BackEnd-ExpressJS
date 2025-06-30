47d06aa607dabf402c5be61d694130649f97cfcf
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-06-30 16:50:34 +0530

    handling request

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
 
-app.use((req,res)=>{
-    console.log("Request is received")
+app.use((req, res) => {
+    // console.log(req);
+    console.log("Request received");
+    // res.send("this is the basic response from the server");
+    // res.send({
+    //     name: "Amman",
+    //     age: 20,
+    //     gender: "Male",
+    //     address: "Bangalore"
+    // });
+    let code = "<h1>Hello World</h1> <ul><li>Jabalpur</li><li>Bhopal</li><li>Nagpur</li><li>Bhilai</li></ul>";
+    res.send(code);
 });
