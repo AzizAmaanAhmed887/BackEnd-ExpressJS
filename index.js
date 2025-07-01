6d47e9c845cfedae095a561af142087fc27f1259
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2025-07-01 16:37:34 +0530

    learn rounting

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
 
-app.use((req, res) => {
-    // console.log(req);
-    console.log("Request received");
-    // res.send("this is the basic response from the server");
-    // res.send({
-    //     name: "Amman",
-    //     age: 20,
-    //     gender: "Male",
-    //     address: "Bangalore"
-    // });
-    let code = "<h1>Hello World</h1> <ul><li>Jabalpur</li><li>Bhopal</li><li>Nagpur</li><li>Bhilai</li></ul>";
-    res.send(code);
-});
+app.get('/home', (req, res) => {
+    res.send("This is GET request for HOME page");
+})
+app.get('/about', (req, res) => {
+    res.send("This is GET request for ABOUT page");
+})
+app.get('/contact', (req, res) => {
+    res.send("This is GET request for CONTACT page");
+})
+app.get('*', (req, res) => { // this isn't working don't know why, but the syntax is correct
+    res.send("This is the default route");
+})
+app.post('/', (req, res) => {
+    res.send("This is POST request");
+})
